24aa1efb1c930ae69622721d3ddf37f89a8dbc0f
Amine <medamineguermezi150@gmail.com>
2026-04-15 22:53:01 +0100

    change Runner

--- src/test/java/runners/AppTest.java
+++ src/test/java/runners/AppTest.java
@@ -1,13 +1,22 @@
 package runners;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class AppTest {
 
-    @Karate.Test
-    Karate runTests() {
-      
-        return Karate.run("classpath:features").tags("~@ignore"); 
+    @Test
+    void testParallel() {
+        // On utilise Runner.path au lieu de Karate.run
+        Results results = Runner.path("classpath:features")
+                .tags("~@ignore") // On conserve ton filtre de tags
+                .parallel(5); // Exécute 5 tests en même temps (accélère ton pipeline !)
+
+        // C'est cette ligne qui fait échouer le test SI ET SEULEMENT SI 
+        // Karate a fini de tout exécuter et de générer karate-summary.html
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
 }
