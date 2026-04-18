b220d74d0cbb531d06dcb33af74ec5aa6b9d44aa
Amine <medamineguermezi150@gmail.com>
2026-04-18 22:18:12 +0100

    2

--- src/test/java/runners/AppTest.java
+++ src/test/java/runners/AppTest.java
@@ -12,7 +12,7 @@ class AppTest {
     @Karate.Test
     Karate testTradeIn() {
         
-        return Karate.run("classpath:features/omni-TradeIn.feature");
+        return Karate.run("classpath:features/omni-DealerLocator.feature");
     }
     */
 
